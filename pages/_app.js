b25531822c42cb9a991c14162bb3ad2e5b13dd65
bensandison <sandisonben@gmail.com>
2022-03-08 20:36:47 +0000

    chakra ui setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import Layout from "../components/layouts";
 
 // Wraps all pages in <Layout>
 function MyApp({ Component, pageProps }) {
 	return (
-		<Layout>
-			<Component {...pageProps} />
-		</Layout>
+		<ChakraProvider>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</ChakraProvider>
 	);
 }
 
