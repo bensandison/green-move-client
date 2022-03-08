520368809f559918ffa59069df141b0833c89d46
bensandison <sandisonben@gmail.com>
2022-03-08 03:59:50 +0000

    added title and nav element

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<nav>
+				<h1>nav here</h1>
+			</nav>
+			<Component {...pageProps} />
+		</>
+	);
 }
 
 export default MyApp;
