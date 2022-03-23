6a76c81a6b4616a61320091249cbb09e304c40f8
bensandison <sandisonben@gmail.com>
2022-03-23 15:26:27 +0000

    index.js redirects to bath page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
 import Head from "next/head";
+import { useEffect } from "react";
+import Router from "next/router";
 
 export default function Home() {
+	useEffect(() => {
+		Router.push("/city/bath");
+	});
 	return (
 		<Head>
 			<title>GreenMove</title>
