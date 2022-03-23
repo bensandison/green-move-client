7132248f86158a96dd89ade571267019266b2f95
bensandison <sandisonben@gmail.com>
2022-03-23 14:32:14 +0000

    export search bar to seperate component

---
+++ components/searchBar.js
@@ -0,0 +1,13 @@
+import { InputGroup, InputRightElement, Input } from "@chakra-ui/react";
+import { SearchIcon } from "@chakra-ui/icons";
+
+export default function SearchBar(props) {
+	return (
+		<InputGroup size="lg">
+			<InputRightElement pointerEvents="none">
+				<SearchIcon />
+			</InputRightElement>
+			<Input type="" placeholder="City Name" value={props.value} />
+		</InputGroup>
+	);
+}
--- pages/city/[cityName].js
+++ pages/city/[cityName].js
@@ -1,20 +1,9 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import {
-	InputGroup,
-	InputRightElement,
-	Input,
-	Flex,
-	Container,
-	AspectRatio,
-	Box,
-	Text,
-	Wrap,
-	SimpleGrid,
-} from "@chakra-ui/react";
-import { SearchIcon } from "@chakra-ui/icons";
+import { Flex, AspectRatio, Text, SimpleGrid } from "@chakra-ui/react";
 import InfoCard from "../../components/infoCard";
+import SearchBar from "../../components/searchBar";
 
 export default function CityData() {
 	const [cityData, setCityData] = useState();
@@ -46,12 +35,7 @@ export default function CityData() {
 	if (!cityData) return <p>loading...</p>;
 	return (
 		<Flex maxW="container.md" direction="column" py={4}>
-			<InputGroup>
-				<InputRightElement pointerEvents="none">
-					<SearchIcon />
-				</InputRightElement>
-				<Input type="" placeholder="City Name" value={cityData.name} />
-			</InputGroup>
+			<SearchBar value={cityData.name}></SearchBar>
 			<AspectRatio my="4" ratio={3 / 1} borderRadius="md">
 				<iframe
 					src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3963.952912260219!2d3.375295414770757!3d6.5276316452784755!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b8b2ae68280c1%3A0xdc9e87a367c3d9cb!2sLagos!5e0!3m2!1sen!2sng!4v1567723392506!5m2!1sen!2sng"
