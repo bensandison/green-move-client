983cb20254db7111a63b0103b65fe51952e2bb71
bensandison <sandisonben@gmail.com>
2022-03-23 17:15:05 +0000

    responsive styles

--- pages/city/[cityName].js
+++ pages/city/[cityName].js
@@ -32,9 +32,9 @@ export default function CityData() {
 	if (!cityData) return <p>loading...</p>;
 	if (cityData) console.log(cityData);
 	return (
-		<Flex maxW="container.md" direction="column" py={4}>
+		<Flex maxW="container.md" direction="column" py={4} m="auto">
 			<SearchBar value={cityData.name}></SearchBar>
-			<AspectRatio my="4" ratio={3 / 1} borderRadius="md">
+			<AspectRatio my="4" ratio={[1, 2 / 1, 3 / 1]} borderRadius="md">
 				<iframe
 					src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3963.952912260219!2d3.375295414770757!3d6.5276316452784755!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b8b2ae68280c1%3A0xdc9e87a367c3d9cb!2sLagos!5e0!3m2!1sen!2sng!4v1567723392506!5m2!1sen!2sng"
 					alt="demo"
@@ -49,7 +49,7 @@ export default function CityData() {
 			<Text fontWeight="semibold" fontSize="lg">
 				Detailed Information
 			</Text>
-			<SimpleGrid my="2" columns={4} spacing={4}>
+			<SimpleGrid my="2" columns={[2, 2, 4]} spacing={4}>
 				<InfoCard title="Air Quality" value={cityData.air_quality}></InfoCard>
 				<InfoCard title="Population" value={cityData.pop}></InfoCard>
 				{/* Some random dummy data... */}
