d9a10cfad08956432ddcf5c4f6298b8212e94553
bensandison <sandisonben@gmail.com>
2022-03-24 15:36:31 +0000

    working search bar

--- components/searchBar.js
+++ components/searchBar.js
@@ -1,21 +1,28 @@
-import { InputGroup, InputRightElement, Input, Button } from "@chakra-ui/react";
+import {
+	InputGroup,
+	InputRightElement,
+	Input,
+	IconButton,
+	Form,
+} from "@chakra-ui/react";
 import { SearchIcon } from "@chakra-ui/icons";
 import { useState } from "react";
+import Router from "next/router";
 
 export default function SearchBar(props) {
 	const [searchQuery, setSearchQuery] = useState("");
 
 	function handleSearchSubmit(e) {
 		e.preventDefault();
-		console.log("sdfhsfdgu");
 
+		// Check search bar has text inside
 		if (searchQuery.replace(/\s/g, "") !== "") {
-			console.log("hellooo");
+			Router.push(`/city/${searchQuery}`);
 		}
 	}
 
 	return (
-		<InputGroup size="lg">
+		<InputGroup as="form" size="lg" onSubmit={handleSearchSubmit}>
 			<Input
 				variant="outline"
 				placeholder={props.value}
@@ -23,13 +30,7 @@ export default function SearchBar(props) {
 				onChange={(e) => setSearchQuery(e.target.value)}
 			/>
 			<InputRightElement>
-				<Button
-					onClick={() => {
-						handleSearchSubmit;
-					}}
-				>
-					<SearchIcon />
-				</Button>
+				<IconButton icon={<SearchIcon />} type="submit"></IconButton>
 			</InputRightElement>
 		</InputGroup>
 	);
