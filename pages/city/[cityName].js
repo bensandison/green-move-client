7f2e4a6dbc514655d84096771299ec10be120599
bensandison <sandisonben@gmail.com>
2022-03-25 16:01:51 +0000

    conditional rendering for data

--- pages/city/[cityName].js
+++ pages/city/[cityName].js
@@ -9,12 +9,20 @@ import {
 	Box,
 	Stack,
 	Heading,
+	useBoolean,
+	Skeleton,
+	Spinner,
+	Progress,
 } from "@chakra-ui/react";
 import InfoCard from "../../components/infoCard";
 import SearchBar from "../../components/searchBar";
 import MultiLeafScore from "../../components/multiLeafScore";
 
 export default function CityData() {
+	// loading + error state for fetch
+	const [loading, setLoading] = useBoolean(true);
+	const [error, setError] = useState();
+
 	const [cityData, setCityData] = useState();
 
 	// Call api with city name when router is ready:
@@ -33,18 +41,24 @@ export default function CityData() {
 			`https://api.greenmove.tk/city/search?name=${cityName}`
 		);
 		let data = await res.json();
-		data = data.data;
-		setCityData(data);
+
+		if (data.status == "success") {
+			setCityData(data.data);
+		} else if (data.status == "fail") {
+			// Set error message state:
+			setError(data.message);
+		}
+
+		return setLoading.off();
 	}
 
-	// If no city data:
-	if (!cityData) return <p>loading...</p>;
 	return (
 		<Flex gap={4} maxW="container.md" direction="column" py={4} m="auto">
-			<SearchBar value={cityData.name}></SearchBar>
+			<SearchBar value={cityData && cityData.name}></SearchBar>
 			<Box>
 				<Heading size="md" fontWeight="medium" pb="2">
-					{`${cityData.name}, ${cityData.county}, ${cityData.country}`}
+					{cityData &&
+						`${cityData.name}, ${cityData.county}, ${cityData.country}`}
 				</Heading>
 				<AspectRatio ratio={[1, 2 / 1, 3 / 1]} borderRadius="md">
 					<iframe
@@ -59,9 +73,9 @@ export default function CityData() {
 					Enviromental Rating
 				</Text>
 				<Stack mt="2" direction="row" spacing={1}>
-					<MultiLeafScore score={cityData.rating}></MultiLeafScore>
+					<MultiLeafScore score={cityData && cityData.rating}></MultiLeafScore>
 					<Text fontWeight="semibold" fontSize="3xl" alignSelf="flex-end">
-						{cityData.rating}
+						{cityData && cityData.rating}
 					</Text>
 				</Stack>
 			</Box>
@@ -70,8 +84,14 @@ export default function CityData() {
 					Detailed Information
 				</Text>
 				<SimpleGrid mt="2" columns={[2, 3, 4]} spacing={4}>
-					<InfoCard title="Air Quality" value={cityData.air_quality}></InfoCard>
-					<InfoCard title="Population" value={cityData.pop}></InfoCard>
+					<InfoCard
+						title="Air Quality"
+						value={cityData && cityData.air_quality}
+					></InfoCard>
+					<InfoCard
+						title="Population"
+						value={cityData && cityData.pop}
+					></InfoCard>
 					{/* Some random dummy data... */}
 					<InfoCard title="Green Space" value="25%"></InfoCard>
 					<InfoCard title="Number Of Parks" value="11"></InfoCard>
