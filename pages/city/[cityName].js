6588a1dcad65b9fe8989a1b604b189060ea5e15e
bensandison <sandisonben@gmail.com>
2022-04-17 14:51:43 +0100

    getStaticPaths frontend

--- pages/city/[cityName].js
+++ pages/city/[cityName].js
@@ -54,61 +54,17 @@ export async function getStaticProps({ params }) {
 	let data = await res.json();
 
 	// Send data to cityData function:
-	return { props: { name: params } };
+	return { props: { cityData: data.data } };
 }
 
-export default function CityData(props) {
-	console.log(props);
-
-	// loading + error state for fetch
-	const [loading, setLoading] = useBoolean(true);
-	const [error, setError] = useState();
-
-	const [cityData, setCityData] = useState();
-
-	// Call api with city name when router is ready:
-	const router = useRouter();
-	useEffect(() => {
-		if (!router.isReady) return;
-
-		// Get data
-		getData();
-	}, [router.isReady]);
-
-	async function getData() {
-		const { cityName } = await router.query;
-
-		const res = await fetch(
-			`https://api.greenmove.tk/city/search?name=${cityName}`
-		);
-		let data = await res.json();
-
-		if (data.status == "success") {
-			setCityData(data.data);
-		} else if (data.status == "fail") {
-			// Set error message state:
-			setError(data.message);
-		}
-
-		return setLoading.off();
-	}
-
-	// Loading screen:
-	if (loading)
-		return (
-			<Flex gap={4} align="center" justify="center" height="90vh">
-				<Spinner size="lg"></Spinner>
-				<Text fontSize="xl">Loading Data...</Text>
-			</Flex>
-		);
-
+export default function CityData({ cityData }) {
 	return (
 		<Flex gap={4} maxW="container.md" direction="column" py={4} m="auto">
 			<Box>
 				<SearchBar
 					value={cityData ? cityData.name : "Enter City Name"}
 				></SearchBar>
-				{error && (
+				{!cityData && (
 					<Alert mt={2} status="error">
 						<AlertIcon></AlertIcon>
 						<AlertTitle>Search Error</AlertTitle>
@@ -116,17 +72,13 @@ export default function CityData(props) {
 					</Alert>
 				)}
 			</Box>
-			{!error && cityData && (
+			{cityData && (
 				<>
 					<Box>
 						<Heading size="md" fontWeight="medium" pb="2">
 							{`${cityData.name}, ${cityData.county}, ${cityData.country}`}
 						</Heading>
 						<AspectRatio ratio={[1, 2 / 1, 3 / 1]} borderRadius="md">
-							{/* <iframe
-								src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3963.952912260219!2d3.375295414770757!3d6.5276316452784755!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b8b2ae68280c1%3A0xdc9e87a367c3d9cb!2sLagos!5e0!3m2!1sen!2sng!4v1567723392506!5m2!1sen!2sng"
-								alt="demo"
-							/> */}
 							<Mapbox
 								longitude={cityData.lng}
 								latitude={cityData.lat}
