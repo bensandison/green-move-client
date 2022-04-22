f23936237f06db6fae6c3f4bf61da7a9686be3e5
calebpercival <56348115+calebpercival@users.noreply.github.com>
2022-04-22 17:51:07 +0100

    Added labels to each place on map
    icon subject to change

--- pages/map.js
+++ pages/map.js
@@ -1,10 +1,28 @@
 import Mapbox from "../components/map";
 
 import { useEffect, useState } from "react";
+import CityData from "./city/[cityName]";
 
-export default function MapPage(){
+export async function getStaticProps() {
+    //get data for all cities
+    const res = await fetch(
+        'https://api.greenmove.io/places/all'
+    )
+
+    const data = await res.json()
+    const allPlaces = data.data
+
+    if(!allPlaces) return { notFound: true }
+
+    console.log(allPlaces)
+
+    return { props: {allPlaces: allPlaces}}
+}
+
+
+export default function MapPage( { allPlaces } ){
 
     return(
-        <Mapbox longitude={3} latitude={55} startingZoom={2} showAll={true}></Mapbox>
+        <Mapbox longitude={3} latitude={55} startingZoom={2} showAll={true} allPlaces={allPlaces}></Mapbox>
     )
 }
