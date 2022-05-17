80511775a39bc72ad656e0cd1edc3af84533cff2
bensandison <sandisonben@gmail.com>
2022-05-17 18:55:19 +0100

    removed max width from layout.js

--- components/layouts.js
+++ components/layouts.js
@@ -1,12 +1,10 @@
-import { Container } from "@chakra-ui/react";
+import { Box, Container } from "@chakra-ui/react";
 
 // Shared layout between multiple pages:
 export default function Layout({ children }) {
 	return (
 		<>
-			<Container as="main" maxW="container.lg">
-				{children}
-			</Container>
+			<Box as="main">{children}</Box>
 		</>
 	);
 }
