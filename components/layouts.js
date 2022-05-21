36ce91b8c9a2ddb2bf77f98cbda2269862d30655
hayhaydz <haydon1900@gmail.com>
2022-05-21 12:17:06 +0100

    SEO and updated analytics to exclude localhost visits

--- components/layouts.js
+++ components/layouts.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Script from 'next/script';
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { Box, Text, Tooltip } from "@chakra-ui/react";
@@ -11,11 +12,21 @@ export default function Layout({ children }) {
 	return (
 		<>
 			<Head>
-				<title>GreenMove.io - LIVE GREEN. MOVE GREEN</title>
-				<meta
-					name="description"
-					content="Providing location based enviromental ratings. Helping you find the greenest place to live."
-				/>
+				// SEO
+				<title>GreenMove.io - LIVE GREEN. MOVE GREEN.</title>
+				<meta name="description" content="Providing location based enviromental ratings. Helping you find the greenest place to live." />
+
+				<meta name="twitter:card" content="summary" />
+				<meta name="twitter:title" content="GreenMove.io - LIVE GREEN. MOVE GREEN." />
+				<meta name="twitter:description" content="Providing location based enviromental ratings. Helping you find the greenest place to live." />
+				<meta name="twitter:image" content="https://greenmove.io/social-image.jpg" />
+
+				<meta property="og:title" content="GreenMove.io - LIVE GREEN. MOVE GREEN." />
+				<meta property="og:type" content="website" />
+				<meta property="og:url" content="https://greenmove.io/" />
+				<meta property="og:image" content="https://greenmove.io/social-image.jpg" />
+				<meta property="og:description" content="Providing location based enviromental ratings. Helping you find the greenest place to live." />
+				<meta property="og:site_name" content="GreenMove.io" />
 				<link
 					rel="apple-touch-icon"
 					sizes="180x180"
@@ -41,8 +52,15 @@ export default function Layout({ children }) {
 				/>
 				<meta name="msapplication-TileColor" content="#ffffff" />
 				<meta name="theme-color" content="#ffffff" />
-				<script async defer data-website-id="d6cd83a0-e485-4dcc-91f0-674c1c6e75a1" src="https://greenmove-stats.herokuapp.com/umami.js"></script>
 			</Head>
+			<Script
+				async
+				defer
+				data-website-id="d6cd83a0-e485-4dcc-91f0-674c1c6e75a1"
+				src="https://greenmove-stats.herokuapp.com/umami.js"
+				data-domains="greenmove.io"
+				strategy="afterInteractive"
+			/>
 			<Box maxW="container.md" py={4} m="auto">
 				<Box
 					as="button"
